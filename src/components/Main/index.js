115af8b0d711684065fd762de95c1beb9a7178c0
Helen <kirren@live.com>
2017-11-23 12:32:15 +0200

    Add better functionality

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -13,33 +13,36 @@ export default {
     toggleNumbers: function (e) {
       let clickedButton = e.target
       if (this.numbers.length < 12) { // array not full
-        // console.log(this.numbers.indexOf(clickedButton.innerText))
+        // console.log(this.numbers.length)
         this.numbers.indexOf(clickedButton.innerText) < 0 ? this.addToSelected(clickedButton) : this.removeFromSelected(clickedButton)
-      } else if (this.numbers.length === 12) {
-        this.colorNumbers()
       } else {
         if (this.numbers.indexOf(clickedButton.innerText) >= 0) {
           this.removeFromSelected(clickedButton)
         }
       }
-      console.log('toggle ' + this.numbers)
     },
     addToSelected: function (item) {
-      if (typeof item === 'number') {
-        this.numbers.push(item)
-      } else if (typeof item === 'object') {
-        this.numbers.push(item.innerText)
+      // pushes to array and highlights
+      this.numbers.push(item.innerText)
+      $(item).addClass('btn-info')
+      console.log('add ' + item.innerText + ' to [' + this.numbers + ']')
+      if (this.numbers.length === 12) {
+        console.log('array is full')
+        this.highlightAllNumbers()
       }
-      this.highlightNumber(item)
-      console.log('add ' + this.numbers)
     },
     removeFromSelected: function (item) {
+      // finds the position in array, removes and unhighlights
       let positionInArray = this.numbers.indexOf(item.innerText)
       this.numbers.splice(positionInArray, 1)
-      this.highlightNumber(item)
-      console.log('remove ' + this.numbers)
+      $(item).removeClass('btn-info btn-success')
+      console.log('remove ' + item.innerText + ' from [' + this.numbers + ']')
+      if (this.numbers.length < 12) {
+        this.unhighlightAllNumbers()
+      }
     },
     randomNumbers: function () {
+      // pushes to array 12 random numbers and highlights them
       if (this.numbers.length > -1) {
         this.deleteNumbers()
       }
@@ -56,33 +59,27 @@ export default {
         })
         i++
       }
-      console.log('rand ' + this.numbers)
+      console.log('randomized: ' + this.numbers)
     },
     deleteNumbers: function () {
       $('.number-btn').removeClass('btn-info btn-success')
       this.numbers = []
-      console.log('delete ' + this.numbers)
+      console.log('delete all from array')
     },
-    colorNumbers: function () {
-      if (this.numbers.length === 12) {
-        $('.btn-info').each(function () {
-          $(this).addClass('btn-success').removeClass('btn-info')
-        })
-      }
+    highlightAllNumbers: function () {
+      $('.btn-info').each(function () {
+        $(this).addClass('btn-success').removeClass('btn-info')
+      })
       console.log('color all ' + this.numbers)
     },
-    highlightNumber: function (button) {
-      console.log('highlight some ' + this.numbers)
-      if (this.numbers.indexOf(button.innerHTML) > 0) {
-        console.log('-' + button.innerHTML + ' is in ' + this.numbers)
-        $(button).addClass('btn-info')
-      } else if (this.numbers.indexOf(button.innerHTML) < 0) {
-        console.log('-' + button.innerHTML + ' is not in ' + this.numbers)
-        $(button).removeClass('btn-info')
-      }
+    unhighlightAllNumbers: function () {
+      $('.btn-success').each(function () {
+        $(this).addClass('btn-info').removeClass('btn-success')
+      })
+      console.log('uncolor all ' + this.numbers)
     },
     saveNumbers: function () {
-      console.log(typeof $('.btn'))
+      // console.log(typeof $('.btn'))
     }
   }
 }
