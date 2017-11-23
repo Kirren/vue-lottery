31d68abc44421bebb6ad2074633f6903d239a9b3
Helen <kirren@live.com>
2017-11-23 18:47:34 +0200

    Add HomePage

---
+++ src/components/HomePage/HomePage.vue
@@ -0,0 +1,5 @@
+<template lang="pug" src="@/components/HomePage/index.pug"></template>
+
+<style lang="scss" src="@/components/HomePage/index.scss" scoped></style>
+
+<script src="./index.js"></script>
---
+++ src/components/HomePage/index.js
@@ -0,0 +1,18 @@
+// import $ from 'jquery'
+import PageHeader from '@/components/PageHeader/PageHeader'
+import LotteryBlock from '@/components/LotteryBlock/LotteryBlock'
+import UsersList from '@/components/UsersList/UsersList'
+import PageFooter from '@/components/PageFooter/PageFooter'
+
+export default {
+  name: 'HomePage',
+  components: {
+    PageHeader,
+    LotteryBlock,
+    UsersList,
+    PageFooter
+  },
+  data () {
+    return {}
+  }
+}
