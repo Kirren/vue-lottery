419b156bd29bf847d8406fab71f944afdb24ee08
Helen <kirren@live.com>
2017-11-23 18:47:20 +0200

    Add PageHeader

---
+++ src/components/PageHeader/PageHeader.vue
@@ -0,0 +1,5 @@
+<template lang="pug" src="@/components/PageHeader/index.pug"></template>
+
+<style lang="scss" src="@/components/PageHeader/index.scss" scoped></style>
+
+<script src="./index.js"></script>
---
+++ src/components/PageHeader/index.js
@@ -0,0 +1,14 @@
+// import $ from 'jquery'
+
+export default {
+  name: 'PageHeader',
+  data () {
+    return {
+      usersAmount: 4779,
+      user: {
+        name: 'Name Lastname',
+        email: 'email@mail.com'
+      }
+    }
+  }
+}
