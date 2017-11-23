45fe04dda8b507530d5731623cb1a1f1fa22b491
Helen <kirren@live.com>
2017-11-23 18:48:27 +0200

    Add test UsersList

---
+++ src/components/UsersList/UsersList.vue
@@ -0,0 +1,5 @@
+<template lang="pug" src="@/components/UsersList/index.pug"></template>
+
+<style lang="scss" src="@/components/UsersList/index.scss" scoped></style>
+
+<script src="./index.js"></script>
---
+++ src/components/UsersList/index.js
@@ -0,0 +1,27 @@
+// import $ from 'jquery'
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+
+export default {
+  name: 'UsersList',
+  data () {
+    return {
+      endpoint: 'https://jsonplaceholder.typicode.com/users',
+      response: [],
+      users: []
+    }
+  },
+  methods: {
+    getPosts: function () {
+      this.$http.get(this.endpoint).then(function (response) {
+        this.response = response.data
+        console.log(this.response)
+      })
+    }
+  },
+  created () {
+    this.getPosts()
+  }
+}
